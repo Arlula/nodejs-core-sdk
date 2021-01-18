29c9f491579b468b8b7296726e2a32a9bb699a37
Scott Owens <scott.owens.dev@gmail.com>
2021-01-18 18:51:30 +1100

    build dependencies and pipeline

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const baseConfig = {
+    entry: {
+        index: "./src/index.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    mode: "development",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    devtool: "source-map",
+    plugins: [],
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+        }]
+    }
+};
+
+module.exports =  [
+    {...baseConfig},
+    {
+        ...baseConfig,
+        mode: "production",
+        output: {
+            filename: "[name].min.js"
+        }
+    }
+];
