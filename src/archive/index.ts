6010acf4469e991828bead654c503e33cb80c54d
Scott Owens <scott.owens.dev@gmail.com>
2021-01-18 18:30:34 +1100

    rewriting archive search and order using axios

--- src/archive/index.ts
+++ src/archive/index.ts
@@ -1,71 +1,45 @@
 import SearchRequest from "./search-request";
 import SearchResult from "./search-result";
 import Order, { fromJSON as OrderFromJSON } from "../orders/order";
-import { client, request, RequestMethod } from "../util/client";
 import OrderRequest from "./order-request";
+import { AxiosInstance } from "axios";
 
-const searchURL = "/api/archive/search";
-const orderURL = "/api/archive/order";
+const searchURL = "https://api.arlula.com/api/archive/search";
+const orderURL = "https://api.arlula.com/api/archive/order";
 
 export default class Archive {
-    private _client: client;
-    constructor(client: client) {
+    private _client: AxiosInstance;
+    constructor(client: AxiosInstance) {
         this._client = client;
     }
 
     Search(req: SearchRequest): Promise<SearchResult[]> {
-        return new Promise((resolve, reject) => {
-            if (!req.valid()) {
-                reject("request not valid");
-                return;
+        if (!req.valid()) {
+            return Promise.reject("request not valid");
+        }
+        return this._client.get(searchURL, {params: req.toQuery()})
+        .then((resp) => {
+            if (!Array.isArray(resp.data)) {
+                return Promise.reject("response was not an array of results");
             }
-            const remoteReq = new request(searchURL);
-            remoteReq.addBatchParams(req.toQuery());
 
-            this._client.do(remoteReq)
-            .then((resp) => {
-                if (!resp.ok()) {
-                    reject(resp.text());
-                    return;
-                }
-                const body = resp.json();
-                if (!Array.isArray(body)) {
-                    reject("response was not an array of results");
-                    return;
-                }
-
-                resolve(body as SearchResult[]);
-            })
-            .catch(reject);
+            return resp.data as SearchResult[];
         });
     }
 
     Order(req: OrderRequest): Promise<Order> {
-        return new Promise((resolve, reject) => {
-            if (!req.valid()) {
-                reject("invalid order request");
-                return;
+        if (!req.valid()) {
+            return Promise.reject("invalid order request");
+        }
+        return this._client.post(orderURL, req.toJSON())
+        .then((resp) => {
+
+            const ord = OrderFromJSON(this._client, resp.data);
+            if (!(ord instanceof Order)) {
+                return Promise.reject(ord);
             }
 
-            const remoteReq = new request(orderURL, RequestMethod.Post)
-            remoteReq.setBody(req.toJSON());
-
-            this._client.do(remoteReq)
-            .then((resp) => {
-                if (resp.ok()) {
-                    reject(resp.text());
-                    return;
-                }
-
-                const ord = OrderFromJSON(this._client, resp.json());
-                if (!(ord instanceof Order)) {
-                    reject(ord);
-                    return;
-                }
-
-                resolve(ord);
-            })
-            .catch(reject);
-        })
+            return ord;
+        });
     }
 }
