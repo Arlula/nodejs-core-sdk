81cd193d6f8a8aad135b37aeae4d1ab86d3ef864
Scott Owens <scott.owens.dev@gmail.com>
2021-01-21 18:06:12 +1100

    webpack only for test builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require("path");
-const { env } = require("process");
 
 const baseConfig = {
     entry: {
-        index: "./src/index.ts",
+        index: "./e2e/index.ts",
     },
     output: {
-        path: path.resolve(__dirname, "build"),
+        path: path.resolve(__dirname, "e2e/dist"),
     },
     mode: "development",
     resolve: {
@@ -14,77 +13,42 @@ const baseConfig = {
     },
     devtool: "source-map",
     plugins: [],
-};
-
-const tsModule = {
-    test: /\.tsx?$/,
-    loader: "ts-loader",
-    exclude: /node_modules/,
-    
-};
-
-const modules = [
-    {
-        ...baseConfig,
-        output: {
-            filename: "browser.js"
-        },
-        module: {
-            rules: [tsModule]
-        }
-    },
-    {
-        ...baseConfig,
-        target: "node",
-        output: {
-            filename: "node.js"
-        },
-        module: {
-            rules: [tsModule]
-        }
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+            options: {
+                configFile: "tests.tsconfig.json"
+            }
+        }]
     }
-];
+};
+
+const outPath = path.resolve(__dirname, "e2e/build");
 
 const testModules = [
     {
         ...baseConfig,
-        entry: {index: "./e2e/index.ts"},
         output: {
-            path: path.resolve(__dirname, "e2e/build"),
+            path: outPath,
             filename: "browser.js"
-        },
-        module: {
-            rules: [{
-                ...tsModule,
-                options: {
-                    configFile: "tests.tsconfig.json"
-                }
-            }]
         }
     },
     {
         ...baseConfig,
         target: "node",
-        entry: {index: "./e2e/index.ts"},
         output: {
-            path: path.resolve(__dirname, "e2e/build"),
+            path: ourPath,
             filename: "node.js"
-        },
-        module: {
-            rules: [{
-                ...tsModule,
-                options: {
-                    configFile: "tests.tsconfig.json"
-                }
-            }]
         }
     }
 ];
 
 module.exports = env => {
     if (env.tests) {
-        modules.push(...testModules);
+        return testModules;
     }
 
-    return modules
+    return [];
 }
