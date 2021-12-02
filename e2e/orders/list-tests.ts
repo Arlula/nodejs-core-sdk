e06531eeed710913277be7bb8b109272b24ca38a
Scott Owens <scott.owens.dev@gmail.com>
2021-12-02 11:43:58 +1100

    improving debug logging and full rejection in order list test

--- e2e/orders/list-tests.ts
+++ e2e/orders/list-tests.ts
@@ -4,20 +4,20 @@ export default function runOrderListTests(client: Arlula): Promise<unknown> {
     return client.orders().list()
     .then((ordList) => {
         if (!ordList.length) {
-            console.log("Orders list empty, expecting existing list");
-            return Promise.reject("expecting existing list");
+            console.log("Order list - list empty, expecting existing list");
+            return Promise.reject("order list - list empty, expecting existing list");
         }
         for (let i=0; i<ordList.length; i++) {
             if (!ordList[i].id) {
-                console.log("received order in list with no ID")
-                return Promise.reject("received order in list with no ID");
+                console.log("order list - received order in list with no ID")
+                return Promise.reject("order list - received order in list with no ID");
             }
         }
         
     })
     .catch((e) => {
-        console.error("unexpected error getting order list: ", e);
-        return Promise.reject(e);
+        console.error("order list - unexpected error getting order list: ", e);
+        return Promise.reject("order list - "+e);
     });
 
 }
