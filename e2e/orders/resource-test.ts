b95a0c7291ee99f402dc65bc9b0f9bfa6c2f2688
Scott Owens <scott.owens.dev@gmail.com>
2021-12-02 12:33:41 +1100

    updating resource download tests for updated signature

--- e2e/orders/resource-test.ts
+++ e2e/orders/resource-test.ts
@@ -3,12 +3,16 @@ import { orderID, resourceID } from "../credentials";
 
 export default function runOrderResourceTests(client: Arlula): Promise<unknown> {
 
-    return Promise.all([test1(client), test2(client)]);
+    return Promise.all([
+        test1(client),
+        test2(client),
+    ]);
 
 }
 
 // order get => child resource => download
 function test1(client: Arlula) {
+    console.log("resource get 1");
     return client.orders().get(orderID)
     .then((order) => {
         if (!order.resources.length) {
@@ -17,10 +21,10 @@ function test1(client: Arlula) {
         }
 
         order.resources.forEach((resource) => {
-            if (resource.type.startsWith("meta_")) {
+            if (resource.type == "meta_json") {
                 resource.download()
                 .then((data) => {
-                    if (!(data instanceof ArrayBuffer || data instanceof Buffer)) {
+                    if (!(data instanceof ArrayBuffer)) {
                         console.error("resource 1, unexpected resource data type: ", arrayBufferToString(data));
                         return Promise.reject(data);
                     }
@@ -40,9 +44,10 @@ function test1(client: Arlula) {
 
 // client => resource download
 function test2(client: Arlula) {
+    console.log("resource get 2");
     return client.orders().downloadResource(resourceID)
     .then((data) => {
-        if (!(data instanceof ArrayBuffer || data instanceof Buffer)) {
+        if (!(data instanceof ArrayBuffer)) {
             console.error("resource 2, unexpected resource data type: ", arrayBufferToString(data));
             return Promise.reject(data);
         }
