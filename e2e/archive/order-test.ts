b07494134b4fe47569235582161c477fca682e84
Scott Owens <scott.owens.dev@gmail.com>
2021-12-02 11:42:42 +1100

    improving error messages in order test

--- e2e/archive/order-test.ts
+++ e2e/archive/order-test.ts
@@ -6,27 +6,30 @@ import { orderingID, orderEULA } from "../credentials";
 
 export default function runOrderTests(client: Arlula): Promise<unknown> {
 
-    return Promise.all([test1(client)]);
+    return Promise.all([
+        test1(client)
+    ]);
 
 }
 
 // basic order
 function test1(client: Arlula) {
+    console.log("order 1");
     const req = new OrderRequest(orderingID, orderEULA, 1);
     return client.archive().order(req)
     .then((resp) => {
         if (!resp.id) {
             console.error("order 1 - Receives order without ID");
-            return Promise.reject("Receives order without ID");
+            return Promise.reject("order 1 - Receives order without ID");
         }
         // pre defined landsat order, will be complete and have resource results
         if (resp.status !== OrderStatus.Complete) {
             console.error("order 1 - order not complete");
-            return Promise.reject("order not complete");
+            return Promise.reject("order 1 - order not complete");
         }
         if (!resp.resources) {
             console.error("order 1 - Landsat order with no resources");
-            return Promise.reject("Landsat order with no resources");
+            return Promise.reject("order 1 - Landsat order with no resources");
         }
     })
     .catch(exceptionHandler("order 1 - basic free"));
