79d3d2df2a9e9ed49c634ad39d5261f69af7ba13
Scott Owens <scott.owens.dev@gmail.com>
2022-07-01 21:27:52 +1000

    including fallback resource location in temp folder for cleanup

--- e2e/orders/resource-test.ts
+++ e2e/orders/resource-test.ts
@@ -77,13 +77,13 @@ function test2(client: Arlula) {
 // client => resource download to file
 function test3(client: Arlula) {
     console.log("resource get 3");
-    return client.orders().downloadResourceToFile(process.env.resource_id || "", process.env.resource_file1 || "res_dl_test_1")
+    return client.orders().downloadResourceToFile(process.env.resource_id || "", process.env.resource_file1 || "temp/res_dl_test_1")
     .then((data) => {
         if (!(data instanceof WriteStream)) {
             console.error("resource 3, unexpected resource data type: ", data);
             return Promise.reject(data);
         }
-        if (data.path != (process.env.resource_file1 || "res_dl_test_1")) {
+        if (data.path != (process.env.resource_file1 || "temp/res_dl_test_1")) {
             console.error("resource 3, unexpected download path: ", data.path);
             return Promise.reject("resource 3, unexpected download path: " + data.path);
         }
@@ -102,7 +102,7 @@ function test3(client: Arlula) {
 // client => resource download to file (with file)
 function test4(client: Arlula) {
     console.log("resource get 4");
-    const file = fs.createWriteStream(process.env.resource_file2 || "res_dl_test_2", { 
+    const file = fs.createWriteStream(process.env.resource_file2 || "temp/res_dl_test_2", { 
         flags: "w+",
         mode: 0o644,
     });
@@ -112,7 +112,7 @@ function test4(client: Arlula) {
             console.error("resource 4, unexpected resource data type: ", data);
             return Promise.reject(data);
         }
-        if (data.path != (process.env.resource_file2 || "res_dl_test_2")) {
+        if (data.path != (process.env.resource_file2 || "temp/res_dl_test_2")) {
             console.error("resource 4, unexpected download path: ", data.path);
             return Promise.reject("resource 4, unexpected download path: " + data.path);
         }
@@ -133,7 +133,7 @@ function test4(client: Arlula) {
 }
 
 function arrayBufferToString(buf: ArrayBuffer): string {
-    // std lib typings enforce an argument of 'number[]' and dont handle
+    // std lib typings enforce an argument of 'number[]' and don't handle
     // the newer numeric array types like Uint8Array 
     // as a result this converts to unknown then to number array for type purposes
     return String.fromCharCode.apply(null, new Uint8Array (buf) as unknown as number[]);
