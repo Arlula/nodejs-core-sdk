a89c7412d92e8a5a31fa14a1ddc009bd26d1ff89
Scott Owens <scott.owens.dev@gmail.com>
2022-07-01 20:49:21 +1000

    order tests for most common errors

--- e2e/archive/order-test.ts
+++ e2e/archive/order-test.ts
@@ -1,11 +1,15 @@
 import Arlula from "../../dist";
 import OrderRequest from "../../dist/archive/order-request";
-import { OrderStatus } from "../../dist/orders/order";
+import Order, { OrderStatus } from "../../dist/orders/order";
 
 export default function runOrderTests(client: Arlula): Promise<unknown> {
 
     return Promise.all([
-        test1(client)
+        test1(client),
+        testError1(client),
+        testError2(client),
+        // TODO: enable this once 2022-07 is live on all servers
+        // testError3(client),
     ]);
 
 }
@@ -36,13 +40,69 @@ function test1(client: Arlula) {
 
 // TODO: add more tests here
 //  - order from search result
-//  - order mismatch eula
-//  - what else?
+
+function testError1(client: Arlula) {
+    console.log("order error 1");
+    const req = new OrderRequest("<invalid>", process.env.order_eula || "", "default");
+    return client.archive().order(req)
+    .then(expectedError("order error 1 - invalid ordering id"))
+    .catch((e) => {
+        if (typeof e !== "string") {
+            console.error("order error 1 - unexpected error: ", e);
+            return Promise.reject("order error 1 - "+e);
+        }
+        if (!e.startsWith("Invalid Ordering ID")) {
+            console.error("order error 1 - Unexpected error response: ", e)
+            return Promise.reject("order error 1 - "+e);
+        }
+    });
+}
+
+function testError2(client: Arlula) {
+    console.log("order error 2");
+    const req = new OrderRequest(process.env.order_key || "", "<invalid>", "default");
+    return client.archive().order(req)
+    .then(expectedError("order error 2 - invalid eula"))
+    .catch((e) => {
+        if (typeof e !== "string") {
+            console.error("order error 2 - unexpected error: ", e);
+            return Promise.reject("order error 2 - "+e);
+        }
+        if (!e.startsWith("You must confirm acceptance of the EULA")) {
+            console.error("order error 2 - Unexpected error response: ", e)
+            return Promise.reject("order error 2 - "+e);
+        }
+    });
+}
+
+function testError3(client: Arlula) {
+    console.log("order error 3");
+    const req = new OrderRequest(process.env.order_key || "", process.env.order_eula || "", "<invalid>");
+    return client.archive().order(req)
+    .then(expectedError("order error 3 - invalid bundle"))
+    .catch((e) => {
+        if (typeof e !== "string") {
+            console.error("order error 3 - unexpected error: ", e);
+            return Promise.reject("order error 3 - "+e);
+        }
+        if (!e.startsWith("Invalid Latitude")) {
+            console.error("order error 3 - Unexpected error response: ", e)
+            return Promise.reject("order error 3 - "+e);
+        }
+    });
+}
 
 
 function exceptionHandler(label: string) {
     return function (e: string) {
         console.error("Error executing " + label + ": ", e);
         return Promise.reject(label+": "+e);
+    }
+}
+
+function expectedError(label: string) {
+    return function (result: Order) {
+        console.error("Error executing"+label+": ", result);
+        return Promise.reject(label+": "+result);
     }
 }
