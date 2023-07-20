0c2bc569ca65e8f8ae01743f618fb9144a8fe266
Scott Owens <sdark.arrow@gmail.com>
2023-07-20 16:44:58 +1000

    implementing collection create update and delete tests

--- e2e/collections/coll-cud.ts
+++ e2e/collections/coll-cud.ts
@@ -3,10 +3,10 @@ import Arlula from "../../dist";
 const tests = [
     test1Create,
     test2Update,
-    test3Delete,
     error1Create,
     error2Update,
     error3Delete,
+    test3Delete,
 ];
 
 export default function runCollectionCUDTests(client: Arlula): Promise<unknown> {
@@ -17,58 +17,88 @@ export default function runCollectionCUDTests(client: Arlula): Promise<unknown> 
 
 }
 
+let savedID = "";
+
 function test1Create(client: Arlula) {
     console.log("coll-cud 1 - create");
-    return client.collections().conformance()
+    return client.collections().create("node SDK test", "testing the node sdk create behavior", [])
     .then((resp) => {
-        // TODO
+        savedID = resp.id;  
+        if (!resp.id) {
+            return Promise.reject("collection cud 1 - returned collection has invalid ID");
+        }
+        if (resp.title != "node SDK test") {
+            return Promise.reject("collection cud 1 - returned collection has incorrect title");
+        }
     })
     .catch(exceptionHandler("collection cud 1 - create"));
 }
 
 function test2Update(client: Arlula) {
     console.log("coll-cud 2 - update");
-    return client.collections().list()
+    if (!savedID) {
+        return Promise.reject("collection cud 2 - no saved ID to update");
+    }
+    return client.collections().update(savedID, "node updated title", "", ["node", "test"])
     .then((resp) => {
-        // TODO
+        if (resp.id != savedID) {
+            return Promise.reject("collection cud 2 - error returned id does not match the expected value");
+        }
+        if (resp.title != "node updated title") {
+            return Promise.reject("collection cud 2 - error returned title does not match the expected value");
+        }
+        if (resp.keywords.length != 2) {
+            return Promise.reject("collection cud 2 - error returned keywords are not of the expected length");
+        }
+        if (resp.keywords[0] != "node" || resp.keywords[1] != "test") {
+            return Promise.reject("collection cud 2 - error returned keywords does not match the expected value");
+        }
     })
     .catch(exceptionHandler("collection cud 2 - update"));
 }
 
 function test3Delete(client: Arlula) {
     console.log("coll-cud 3 - delete");
-    return client.collections().get("")
-    .then((resp) => {
-        // TODO
-    })
+    if (!savedID) {
+        return Promise.reject("collection cud 2 - no saved ID to delete");
+    }
+    return client.collections().delete(savedID)
     .catch(exceptionHandler("collection cud 3 - delete"));
 }
 
 function error1Create(client: Arlula) {
     console.log("coll-get error 1 - not found error");
-    return client.collections().get("")
-    .then((resp) => {
-        // TODO
-    })
-    .catch(exceptionHandler("collection get error 1 - create"));
+    return client.collections().create("!!!!!!`;", "", [])
+    .then(expectedError("collection cud error 2 - update"))
+    .catch((e) => {
+        if (!e || !e.startsWith("Invalid collection title")) {
+            console.error("collection cud error 1 - create: Unexpected error response: ", e)
+            return Promise.reject("collection cud error 1 - create unexpected error: "+e);
+        }
+    });
 }
 
 function error2Update(client: Arlula) {
     console.log("coll-cud error 2 - update");
-    return client.collections().get("")
-    .then((resp) => {
-        // TODO
-    })
-    .catch(exceptionHandler("collection cud error 2 - update"));
+    return client.collections().update(savedID, "!!!!!!`;", "", [])
+    .then(expectedError("collection cud error 2 - update"))
+    .catch((e) => {
+        if (!e || !e.startsWith("Invalid collection title")) {
+            console.error("collection cud error 2 - update: Unexpected error response: ", e)
+            return Promise.reject("collection cud error 2 - update unexpected error: "+e);
+        }
+    });
 }
 
 function error3Delete(client: Arlula) {
     console.log("coll-cud error 3 - delete");
-    return client.collections().get("")
-    .then((resp) => {
-        // TODO
-    })
-    .catch(exceptionHandler("collection cud error 3 - delete"));
+    return client.collections().delete("")
+    .catch((e) => {
+        if (!e || !e.startsWith("404 page not found")) {
+            console.error("collection cud error 3 - delete not found error: Unexpected error response: ", e)
+            return Promise.reject("collection cud error 3 - delete not found error: "+e);
+        }
+    });
 }
 
 function exceptionHandler(label: string) {
