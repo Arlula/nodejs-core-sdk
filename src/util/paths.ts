1a5cb66a31d2642fcb11e474c6ed16f3c451f220
Scott Owens <scott.owens.dev@gmail.com>
2023-08-23 14:48:08 +1000

    adding spaces between path's sections for clarity

--- src/util/paths.ts
+++ src/util/paths.ts
@@ -8,14 +8,17 @@ export default class paths {
     static get Test(): string {
         return host+"/api/test";
     }
+
     static get ArchiveSearch(): string {
         return host + "/api/archive/search";
     }
     static get ArchiveOrder(): string {
         return host + "/api/archive/order";
-    }static get ArchiveOrderBatch(): string {
+    }
+    static get ArchiveOrderBatch(): string {
         return host + "/api/archive/order/batch";
     }
+
     static get OrderList(): string {
         return host + "/api/order/list";
     }
